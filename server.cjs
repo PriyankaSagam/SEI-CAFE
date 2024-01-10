298d72700294f47720c75bcc1eba1e7d479a7b23
venupriya1 <priyanikavenu@gmail.com>
2024-01-09 19:56:40 -0800

    added components,pages,routes,models,controllers and utilities

--- server.cjs
+++ server.cjs
@@ -38,9 +38,14 @@ app.get('/api/test', (req, res) => {
   });
 
 app.use('/api/users', require('./routes/api/users.cjs'));
-// we have included the line 
+// we have included the line
 // const userRouter = require('./routes/api/users.cjs')
 // app.use('/api/user', userRouter);
+
+// Protect the API routes below from anonymous users
+const ensureLoggedIn = require('./config/ensureLoggedIn.cjs');
+app.use('/api/items', ensureLoggedIn, require('./routes/api/items.cjs'));
+app.use('/api/orders', ensureLoggedIn, require('./routes/api/orders.cjs'));
 
 // The following "catch all" route (note the /*) is necessary
 // to return the index.html on all non-AJAX requests
